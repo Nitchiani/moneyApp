3203bec58d3dffd35065b82be46be1be89e258b0
Nitchiani <gordiashvilinini@gmail.com>
2025-06-06 14:22:50 +0400

    done welcome page

---
+++ app/index.tsx
@@ -0,0 +1,39 @@
+import Buttons from "@/components/Buttons";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+const { width: screenWidth } = Dimensions.get("window");
+
+const DESIGN_WIDTH = 393;
+const DESIGN_HEIGHT = 526;
+
+const calculatedHeight = (DESIGN_HEIGHT / DESIGN_WIDTH) * screenWidth;
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          source={require("../assets/images/background1.png")}
+          style={styles.backgroundImage}
+        />
+      </View>
+      <View style={styles.buttonsWrapper}>
+        <Buttons text="Sign In" variant="blue" />
+        <Buttons text="Sign Up" variant="transparent" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    resizeMode: "cover",
+    width: screenWidth,
+    height: calculatedHeight,
+  },
+  container: {
+    gap: 80,
+  },
+  buttonsWrapper: {
+    gap: 26,
+  },
+});
---
+++ components/Buttons.tsx
@@ -0,0 +1,90 @@
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const CustomButton = ({ text, variant = "blue" }) => {
+  const isBlue = variant === "blue";
+
+  return (
+    <TouchableOpacity style={styles.buttonWrapper}>
+      {isBlue ? (
+        <LinearGradient
+          colors={["#6075FF", "#1433FF"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          style={styles.button}
+        >
+          <View style={styles.buttonContent}>
+            <Text style={styles.blueText}>{text}</Text>
+            <Image
+              source={require("../assets/images/whiteArrow.png")}
+              style={styles.arrow}
+            />
+          </View>
+          <Image
+            source={require("../assets/images/topMask.png")}
+            style={styles.mask}
+          />
+        </LinearGradient>
+      ) : (
+        <View style={[styles.button, styles.transparentButton]}>
+          <View style={styles.buttonContent}>
+            <Text style={styles.transparentText}>{text}</Text>
+            <Image
+              source={require("../assets/images/blueArrow.png")}
+              style={styles.arrow}
+            />
+          </View>
+          <Image
+            source={require("../assets/images/bottomMask.png")}
+            style={styles.mask}
+          />
+        </View>
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
+
+const styles = StyleSheet.create({
+  buttonWrapper: {
+    marginHorizontal: 32,
+  },
+  button: {
+    padding: 24,
+    borderRadius: 28,
+    position: "relative",
+    overflow: "hidden",
+  },
+  transparentButton: {
+    borderWidth: 1,
+    borderColor: "#556BFF",
+  },
+  buttonContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  blueText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "400",
+  },
+  transparentText: {
+    color: "#556BFF",
+    fontSize: 20,
+    fontWeight: "400",
+  },
+  arrow: {
+    width: 21,
+    height: 17,
+  },
+  mask: {
+    position: "absolute",
+    width: 327,
+    height: 75,
+    top: 0,
+    right: 0,
+    zIndex: 1,
+  },
+});
